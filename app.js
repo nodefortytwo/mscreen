b112bd4831a48d9188080ccf58cb870d67fc7d27
Rick Burgess <git@rickburgess.me>
2012-11-18 11:47:56 +0000

    added nowjs

--- app.js
+++ app.js
@@ -22,12 +22,34 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+
+
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+
+//heroku doesn't yet support proper web sockets so we have to use long polling instead
+var nowjs = require("now");
+var everyone = nowjs.initialize(server, {socketio: {transports: ['xhr-polling', 'jsonp-polling']}});
+
+//nowstuff
+var usrCnt = 0;
+
+nowjs.on('connect', function() {
+    usrCnt++;
+    console.log('User Connected');
+    console.log('User Count: ' + usrCnt);
+});
+
+nowjs.on('disconnect', function() {
+    usrCnt--;
+    console.log('User Disconnected');
+    console.log('User Count: ' + usrCnt);
 });
